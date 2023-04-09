467a6690ed7c6e0d1a047e578d9ed872d025038a
wahid9000 <wahidchowdhury9000@gmail.com>
2023-04-10 00:02:19 +0600

    initial setup done and added navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div className='px-2 lg:px-36'>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default App;
