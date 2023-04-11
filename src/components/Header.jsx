b8f92aba717785e53f0c0a3ddf808ee2bfeb7367
wahid9000 <wahidchowdhury9000@gmail.com>
2023-04-11 23:18:31 +0600

    active link functionality implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ActiveLink from '../utils/ActiveLink';
 
 const Header = () => {
     return (
@@ -11,20 +12,20 @@ const Header = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/applied">Applied Jobs</Link></li>
-                            <li><Link to="/stat">Statistics</Link></li>
-                            <li><Link to="/blogs">Blog</Link></li>
+                            <li><ActiveLink to="/">Home</ActiveLink></li>
+                            <li><ActiveLink to="/applied">Applied Jobs</ActiveLink></li>
+                            <li><ActiveLink to="/stat">Statistics</ActiveLink></li>
+                            <li><ActiveLink to="/blogs">Blog</ActiveLink></li>
                         </ul>
                     </div>
                     <Link to="/" className="btn btn-ghost normal-case text-xl">JobsCorner</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/applied">Applied Jobs</Link></li>
-                        <li><Link to="/stat">Statistics</Link></li>
-                        <li><Link to="/blogs">Blog</Link></li>
+                        <li><ActiveLink to="/">Home</ActiveLink></li>
+                        <li><ActiveLink to="/applied">Applied Jobs</ActiveLink></li>
+                        <li><ActiveLink to="/stat">Statistics</ActiveLink></li>
+                        <li><ActiveLink to="/blogs">Blog</ActiveLink></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
